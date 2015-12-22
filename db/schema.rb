7bf6b2c2a0d9d60d572ac9632375552edb8477c5
Fellipe Brito <fellipe@devlandia.net>
2015-12-22 23:44:12 +0000

    Adds Images

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222224831) do
+ActiveRecord::Schema.define(version: 20151222233958) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      null: false
     t.text     "content",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "filepath",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
