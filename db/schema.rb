1c958c3580458da8d4bf26423464501f6b0fd4cf
Fellipe Brito <fellipe@devlandia.net>
2015-12-22 23:09:26 +0000

    adds article create and show
    refactored and done
    
    refactor
    
    routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222211715) do
+ActiveRecord::Schema.define(version: 20151222224831) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.text     "content",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
