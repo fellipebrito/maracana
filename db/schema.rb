7f5b696a38accf27873e81ca5977bd541f66c45c
Fellipe Brito <fellipe@devlandia.net>
2015-12-23 02:41:16 +0000

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222233958) do
+ActiveRecord::Schema.define(version: 20151223021535) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      null: false
     t.text     "content",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "images", force: :cascade do |t|
     t.string   "title",      null: false
--- spec/features/user_creates_an_article_spec.rb
+++ spec/features/user_creates_an_article_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 feature 'User creates an article with valid data' do
   scenario 'they fill the form, post and sees the article saved' do
+    sign_up_with email: 'lucky@skywalker.com', password: 'password'
     submit_article title: 'The force awakens', content: 'May the 4th be with u'
 
     expect(page).to have_text('The force awakens')
+    expect(page).to have_text('by lucky@skywalker.com')
     expect(page).to have_text('May the 4th be with u')
     expect(page).to have_link('Edit')
   end
