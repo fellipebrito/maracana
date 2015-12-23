12e8172bfcba48f60137054c70c142305bc765a4
Fellipe Brito <fellipe@devlandia.net>
2015-12-23 01:22:04 +0000

    Test basic sign up/in

---
+++ spec/features/visitor_signs_up_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'Visitor signs up' do
+  scenario 'with valid email and password' do
+    sign_up_with 'valid@example.com', 'password'
+
+    expect(page).to have_selector(:link_or_button, 'Sign out')
+  end
+
+  scenario 'with invalid email' do
+    sign_up_with 'invalid_email', 'password'
+
+    expect(page).to have_content('Sign in')
+  end
+
+  scenario 'with blank password' do
+    sign_up_with 'valid@example.com', ''
+
+    expect(page).to have_content('Sign in')
+  end
+end
